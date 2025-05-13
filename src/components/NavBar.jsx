db8dcfa6035fc410ad217cc763a2a5475fe85bd0
kingwell47 <joeldoctor47@gmail.com>
2025-05-13 14:48:59 +0800

    some few updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = () => {
   const { user, logout } = useAuthStore();
 
   return (
-    <nav className='navbar bg-base-100 shadow-sm'>
-      <div className='flex-1'>
-        <a className='btn btn-ghost text-xl'>HabiTrack</a>
+    <nav className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">HabiTrack</a>
       </div>
-      <div className='flex-none'>
-        <ul className='menu menu-horizontal px-1'>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
           {user && (
             <>
-              <li className='hidden md:visible'>
+              <li className="invisible md:visible">
                 <a>{user.email}</a>
               </li>
               <li>
